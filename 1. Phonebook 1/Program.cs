0f640ccd39e72261568e324ea660834f42be0c82
sapa73 <34308956+sapa73@users.noreply.github.com>
2018-02-13 11:12:22 +0200

    Dictionaries

---
+++ 1. Phonebook 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Dynamic;
+using System.Linq;
+
+namespace _1._Phonebook_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string,string> phonebook=new Dictionary<string, string>();
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] phonebookParameters = input.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                string command = phonebookParameters[0];
+                if (command == "A")
+                {
+                    string key = phonebookParameters[1];
+                    string value = phonebookParameters[2];
+                    phonebook[key] = value;
+                }
+                else
+                {
+                    string key = phonebookParameters[1];
+                    if (phonebook.ContainsKey(key))
+                    {
+                              Console.WriteLine($"{key} -> {phonebook[key]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {key} does not exist.");
+
+                    }
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
---
+++ 3. A Miner Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
