0f5abb0e8d64b207210e7bcd7dfc778be53b9e7f
sapa73 <34308956+sapa73@users.noreply.github.com>
2018-02-15 18:49:47 +0200

    adding

---
+++ 8.  Logs Aggregator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8.__Logs_Aggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
